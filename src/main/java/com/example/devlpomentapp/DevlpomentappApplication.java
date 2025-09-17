036ba35566f482913018c18291e76b4b0e97f349
Varsini <sjvarsini@gmail.comgit config --global user.name Varsini$ git config --global user.email sjvarsini@gmail.com>
2025-09-17 09:51:37 +0530

    sec commit

--- src/main/java/com/example/devlpomentapp/DevlpomentappApplication.java
+++ src/main/java/com/example/devlpomentapp/DevlpomentappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevlpomentappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello world");
 		SpringApplication.run(DevlpomentappApplication.class, args);
 	}
 
