e099050b402107499db23c211890f9628937c595
Varsini <sjvarsini@gmail.comgit config --global user.name Varsini$ git config --global user.email sjvarsini@gmail.com>
2025-09-17 10:43:44 +0530

    main change

--- src/main/java/com/example/devlpomentapp/DevlpomentappApplication.java
+++ src/main/java/com/example/devlpomentapp/DevlpomentappApplication.java
@@ -8,6 +8,7 @@ public class DevlpomentappApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello new world");
 		SpringApplication.run(DevlpomentappApplication.class, args);
 	}
 
